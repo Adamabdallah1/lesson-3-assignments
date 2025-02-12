6dc1db0794ae7744c2d766058bc0e8424b7a46ea
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-12 20:37:43 +0200

    Adding quit option for user to stop program

--- even_odd_checker.py
+++ even_odd_checker.py
@@ -1,7 +1,7 @@
 #Exercise 2: Even or Odd Number Checker
 
 #Write a Python program that asks the user to enter a number and checks whether it is even or odd.
-#Example:
+    #Example:
 #Enter a number: 7
 #The number is odd.
 #Enter a number: 10
@@ -11,14 +11,21 @@
 
 #Ask the user to enter a number.
 
-while True:
+code = True
+
+while code:
     def even_odd_checker(number):
+        
         if number % 2 == 0:
             return "The number is even"
         else:
-            return "The number is odd" 
-    
+            return "The number is odd"
+
     number = int(input("Enter a number: "))
     results = even_odd_checker(number)
-
     print(results)
+    user_input = input("Enter [Q] to quit or any key to continue: ")
+
+    if user_input.upper() == "Q":
+        print("Goodbye!")
+        code = False
