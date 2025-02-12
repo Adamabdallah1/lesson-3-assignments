93f06967e0f275dc698574129b466a5c4318701f
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-02-12 19:58:04 +0200

    Creating file for python code to check price of movies by age

---
+++ movie_ticket_calc.py
@@ -0,0 +1,8 @@
+#1) Exercise 1: Movie Ticket Price CalculatorWrite a Python program that asks the user for their age and determines the price of a movie ticket based on the following rules:
+#- If the user is , the ticket is
+#- If the user is , the ticket costs .
+#- If the user is , the ticket costs .
+#- If the user is , the ticket costs .
+#Example:
+#Enter your age: 8
+#The ticket price is $5.
